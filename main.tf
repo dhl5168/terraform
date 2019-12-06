e13b8cb16a66d0a431484accf1adeb7728e8d452
dhl5168 <dhl5168@outlook.com>
2019-12-06 15:03:14 +0800

    enable ssh login

--- main.tf
+++ main.tf
@@ -5,4 +5,19 @@ provider "aws"{
 resource "aws_instance" "web" {
   ami = "ami-0d59ddf55cdda6e21"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.ssh.key_name
+}
+
+resource "aws_key_pair" "ssh" {
+  key_name   = "admin"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeAav2bZTf/4CaW3vDoBQTTlpGtzileCPU2dcJUR2xZCpr25b1ybR2hUibukfYUHzb+ZfhEFiTTgp6LbGJTwHshUUmFNqRGbCEk+W9fRErhY0kE8rXlRSXXo5JqX2BYbmSQCmRfTitYTVCIVWopekKhabndkOgHWd6EGokXWOhKPo+HegoX1MLuW6hyNq1cEeM8j/A9V8F0MkuusDOyf2TalNMue3EZ8MlijqDSpJ2tg3IB3hZOBz0BKrRAJed4Whz4tzuwhNYgvgoaa9X2aXMkfZINGqMmEI90EmRC9dCkzPSTCwusWRNyABcEyNrsoie0a7DVkbhIrq87Ncynvt9 dennis@EPCNSZXW0188"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type          = "ingress"
+  from_port     = 22
+  to_port       = 22
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  security_group_id = "sg-asdf2342"
 }
